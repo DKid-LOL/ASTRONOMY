94068048ac9d6f9c67fd8853cdc6a8224a20ddef
nguyenngocphong98 <nguyenngocphong9898@gmail.com>
2024-04-25 22:29:16 +0700

    đài quan sát

--- DoAnKi1/database/migrations/2024_04_22_185920_create_observatory_table.php
+++ DoAnKi1/database/migrations/2024_04_22_185920_create_observatory_table.php
@@ -13,6 +13,19 @@ return new class extends Migration
     {
         Schema::create('observatory', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('Telescope'); // kính thiên văn
+            $table->string('Dome');
+            $table->string('Control room'); // phòng điều khiển
+            $table->string('Satellite'); // vệ tinh
+            $table->string('Space telescope'); // kính thiên văn không gian
+            $table->string('Data transmission system'); // hệ thống truyền dữ liệu
+            $table->string('Radio antenna'); // Ăng-ten radio
+            $table->string('Antenna array'); // Mảng ăng-ten
+            $table->string('Signal processor'); // Bộ xử lý tín hiệu
+            $table->string('Special filter'); // Kính lọc đặc biệt
+            $table->string('Spectrum analyzers'); // các thiết bị phân tích phổ
+            $table->string('Accurate tracking system'); // hệ thống theo dõi chính xác
             $table->timestamps();
         });
     }
