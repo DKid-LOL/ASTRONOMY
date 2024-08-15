680bad1e3b7e0891dddddb304ff031bf4b0bcaeb
Chokominto <67826817+DKid-LOL@users.noreply.github.com>
2024-08-15 15:27:30 +0700

    Delete DoAnKi1 directory

--- DoAnKi1/public/js/navbar.js
+++
@@ -1,8 +0,0 @@
-
-$(function() {
-    $('.navbar-toggler').on('click', function() {
-        $('.collapse').collapse('toggle');
-    });
-});
-console.log($); // Kiểm tra xem jQuery đã sẵn sàng hay chưa
-
