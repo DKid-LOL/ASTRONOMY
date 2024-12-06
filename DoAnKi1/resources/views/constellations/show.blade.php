f16a8478d4077b6c99d420387b006469337fc075
Chokominto <hero091205@gmail.com>
2024-12-07 06:43:12 +0700

    update

--- DoAnKi1/resources/views/constellations/show.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <h1>{{ $constellation->name }}</h1>
-    <p>Mô tả: {{ $constellation->description }}</p>
-    <p>Làm thế nào nó được hình thành: {{ $constellation->formation }}</p>
-</div>
-@endsection
